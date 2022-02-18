055a8120a4f7cf6f975853be9a9a29d19c2baf0d
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-02-18 15:50:49 -0300

    Estruturas de dados em Java: Listas - Implementação do método toString

--- Estrutura de Dados em Java/Estruturas de Dados em Java Listas/src/main/java/projeto/listaencadeada/ListaEncadeada.java
+++ Estrutura de Dados em Java/Estruturas de Dados em Java Listas/src/main/java/projeto/listaencadeada/ListaEncadeada.java
@@ -24,6 +24,10 @@ public class ListaEncadeada <T> {
 
     }
 
+    public T get(int index){
+        return getNo(index).getConteudo();
+    }
+
     private No<T> getNo(int index){
 
         validaIndice(index);
@@ -35,6 +39,19 @@ public class ListaEncadeada <T> {
             noAuxiliar = noAuxiliar.getProximoNo();
         }
         return noRetorno;
+    }
+
+    public T remove(int index){
+        No<T> noPivor = this.getNo(index);
+        if(index == 0){
+            referenciaEntrada = noPivor.getProximoNo();
+            return noPivor.getConteudo();
+        }
+
+        No<T> noAnterior = getNo(index - 1);
+        noAnterior.setProximoNo(noPivor.getProximoNo());
+        return noPivor.getConteudo();
+
     }
 
 
@@ -66,5 +83,17 @@ public class ListaEncadeada <T> {
 
     public boolean isEmpty(){
         return referenciaEntrada == null ? true : false;
+    }
+
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        No<T> noAuxiliar = referenciaEntrada;
+        for(int i = 0; i < this.size(); i++){
+            strRetorno += "[No{conteudo=" + noAuxiliar.getConteudo() +"}]--->";
+            noAuxiliar = noAuxiliar.getProximoNo();
+        }
+        strRetorno += "null";
+        return strRetorno;
     }
 }
