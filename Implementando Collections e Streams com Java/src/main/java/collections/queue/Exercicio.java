629bb3ce493e1e6013eac6381a71264833ed6320
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-02-28 18:40:43 -0300

    Implementando Collections e Streams com Java - QUEUE

---
+++ Implementando Collections e Streams com Java/src/main/java/collections/queue/Exercicio.java
@@ -0,0 +1,44 @@
+package collections.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Exercicio {
+    public static void main(String[] args) {
+
+        Queue<String> minhaFila = new LinkedList<>();
+
+        minhaFila.add("Juliana");
+        minhaFila.add("Pedro");
+        minhaFila.add("Carlos");
+        minhaFila.add("Larissa");
+        minhaFila.add("João");
+
+        System.out.println(minhaFila);
+
+        String filaSemRemover = minhaFila.peek();
+
+        System.out.println(filaSemRemover);
+
+        System.out.println(minhaFila);
+
+        String filaRemovendo = minhaFila.poll();
+
+        System.out.println(filaRemovendo);
+
+        System.out.println(minhaFila);
+
+        minhaFila.add("Daniel");
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.size());
+
+        System.out.println(minhaFila.isEmpty());
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.contains("Carlos"));
+        
+    }
+}
